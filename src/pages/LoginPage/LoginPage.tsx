8e54531d62c3d563186636689ba0180deb97e6e1
Christian Kongsgaard <kongsgaard.c@gmail.com>
2024-02-04 09:34:19 +0100

    auth for github

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,11 +1,12 @@
-import { Container } from '@mantine/core'
+import { Button, Container, Group, TextInput, Title } from '@mantine/core'
 import { useAuth } from '@contexts'
 import { useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export const LoginPage = () => {
   const { token } = useAuth()
   const navigate = useNavigate()
+  const [ value, setValue] = useState<string | null>('')
 
   useEffect(() => {
     if (token) {
@@ -16,7 +17,10 @@ export const LoginPage = () => {
 
   return (
     <Container>
-      HELLO FROM LOGIN
+      <Title>BOOM - Bill Of Open Materials</Title>
+      <br />
+      {/*@ts-ignore*/}
+      <Group><TextInput placeholder='Auth code from Speckle' value={value} setValue={setValue}/><Button onClick={() => navigate(`/auth?${value}`)}>Go</Button></Group>
     </Container>
   )
 }
